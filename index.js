cdd730851dc04da0c0934317f2c03372ced6f116
Justin Sisley <sisley.justin@gmail.com>
2019-03-31 16:04:54 -0700

    Now using identity-proxy; added tests; updated docs

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-var babelJest = require('babel-jest');
-
-module.exports = {
-	process: function(src, filename) {
-		if (filename.match(/\.(css|less|scss|styl|sss)$/)) {
-			return '';
-		}
-
-		return babelJest.process(src, filename);
-	},
-};
+module.exports = require('identity-obj-proxy');
